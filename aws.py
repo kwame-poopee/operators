85ecc14c0da8e4e77c0b22fcfdcc70dc4bf9c9aa
Samuel K. Frimpong <72517937+kwame-poopee@users.noreply.github.com>
2020-11-19 09:21:21 +0000

    Add files via upload

---
+++ aws.py
@@ -0,0 +1,30 @@
+#mathematical operations
+num1 = 23 
+num2 = 2 
+total = num1 + num2
+print( "total", total)
+product = num1 * num2
+print(product)
+change = num1 - num2 
+print(change)
+divisibility = num1/num2 
+print(divisibility)
+modulo = num1 % num2 
+print(modulo)
+
+def sub(n):
+    sub = (2**n) - 1 
+    print(sub)
+
+sub(4)
+
+
+
+
+
+
+
+
+
+
+
